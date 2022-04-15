b5ac8e037bc58f4fdc3486bac8d86de1dbcaba13
krummitz <amidbidee@gmail.com>
2022-04-15 02:44:09 +0100

    task 1 - 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module
+
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return floor of function
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module
+
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns string rep of n
+    """
+    return str(n)
