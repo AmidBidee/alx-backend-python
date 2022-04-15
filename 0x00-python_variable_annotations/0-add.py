c0400d1c7f5128300f9ebad1cbabc96ebb37fa18
krummitz <amidbidee@gmail.com>
2022-04-15 02:04:17 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module
+
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns sum of a and b
+
+    Args:
+        a [float]: float a to be summed
+        b [float]: float b to be summed
+
+    Return: flot -> sum of a and b
+    """
+    return a + b
